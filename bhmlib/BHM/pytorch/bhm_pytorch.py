3c338d1003176ce1f9afb716ab3c93b5c3e5f424
Emma Pinegar <u1008834@utah.edu>
2026-01-30 10:24:03 -0700

    updating BHM for stein purposes

--- bhmlib/BHM/pytorch/bhm_pytorch.py
+++ bhmlib/BHM/pytorch/bhm_pytorch.py
@@ -15,7 +15,7 @@ from .kernels import RBF
 
 dtype_ = pt.float32
 device_ = pt.device("cuda:0" if pt.cuda.is_available() else "cpu")
-EXPANSION_COEF = 0.01
+EXPANSION_COEF = 0.05
 VERBOSE = True
 
 #TODO: merge 2D and 3D classes into a single class
@@ -27,7 +27,7 @@ VERBOSE = True
 
 
 class BHM_PYTORCH():
-    def __init__(self, gamma=0.05, grid=None, cell_resolution=(5, 5), cell_max_min=None, X=None, nIter=0, mu_sig=None, mu=None, sig=None, epsilon=None, torch_kernel_func=False, device=device_):
+    def __init__(self, gamma=0.05, grid=None, cell_resolution=(5, 5), cell_max_min=None, X=None, nIter=0, mu_sig=None, mu=None, sig=None, epsilon=None, torch_kernel_func=False, device=device_, file=None):
         """
         :param gamma: RBF bandwidth
         :param grid: if there are prespecified locations to hinge the RBF
@@ -36,27 +36,31 @@ class BHM_PYTORCH():
         :param X: a sample of lidar locations to use when both 'grid' and 'cell_max_min' are 'None'
         """
         self.device = device
-        self.gamma = gamma
-        if grid is not None:
-            self.updateGrid(grid)
+        self.limits = cell_max_min
+        if file is not None:
+            self.load(file)
         else:
-            if (X is not None and X.shape[1] > 2) or (cell_max_min is not None and len(cell_max_min) > 4):
-                self.grid = self.__calc_3d_grid_auto(cell_resolution, cell_max_min, X)
+            self.gamma = gamma
+            if grid is not None:
+                self.updateGrid(grid)
             else:
-                self.grid = self.__calc_2d_grid_auto(cell_resolution, cell_max_min, X)
+                if (X is not None and X.shape[1] > 2) or (cell_max_min is not None and len(cell_max_min) > 4):
+                    self.grid = self.__calc_3d_grid_auto(cell_resolution, cell_max_min, X)
+                else:
+                    self.grid = self.__calc_2d_grid_auto(cell_resolution, cell_max_min, X)
 
-        self.nIter = nIter
-        if VERBOSE:
-            print(f" Number of hinge points={self.grid.shape[0]}")
+            self.nIter = nIter
+            if VERBOSE:
+                print(f" Number of hinge points={self.grid.shape[0]}")
 
-        #ADDED
-        if mu_sig is not None:
-            self.updateMuSig(mu_sig)
-        else:
-            if mu is not None:
-                self.mu = mu
-            if sig is not None:
-                self.sig = sig
+            #ADDED
+            if mu_sig is not None:
+                self.updateMuSig(mu_sig)
+            else:
+                if mu is not None:
+                    self.mu = mu
+                if sig is not None:
+                    self.sig = sig
         self.torch_kernel_func = torch_kernel_func
         if torch_kernel_func:
             self.rbf_torch = RBF()
@@ -111,7 +115,15 @@ class BHM_PYTORCH():
             # if 'max_min' is not given, make a boundarary based on X
             # assume 'X' contains samples from the entire area
             x_min, x_max, y_min, y_max, z_min, z_max = X[:, 0].min(), X[:, 0].max(), X[:, 1].min(), X[:, 1].max(), X[:, 2].min(), X[:, 2].max()
-            x_expansion, y_expansion, z_expansion = (x_max - x_min) * EXPANSION_COEF, (y_max - y_min) * EXPANSION_COEF, (z_max - z_min) * EXPANSION_COEF
+            x_range, y_range, z_range = (x_max - x_min), (y_max - y_min), (z_max - z_min)
+
+            # x_middle, y_middle, z_middle = x_range/2 + x_min, y_range/2 + y_min, z_range/2 + z_min
+            # x_points, y_points, z_points = pt.round(x_range/cell_resolution[0] + 1.5)*cell_resolution[0]/2, pt.round(y_range/cell_resolution[1] + 1.5)*cell_resolution[1]/2, pt.round(z_range/cell_resolution[2] + 1.5)*cell_resolution[2]/2
+            # x_min, x_max = x_middle - x_points, x_middle + x_points + cell_resolution[0]
+            # y_min, y_max = y_middle - y_points, y_middle + y_points + cell_resolution[1]
+            # z_min, z_max = z_middle - z_points, z_middle + z_points + cell_resolution[2]
+
+            x_expansion, y_expansion, z_expansion = x_range * EXPANSION_COEF, y_range * EXPANSION_COEF, z_range * EXPANSION_COEF
             x_min, x_max = x_min - x_expansion, x_max + x_expansion
             y_min, y_max = y_min - y_expansion, y_max + y_expansion
             z_min, z_max = z_min - z_expansion, z_max + z_expansion
@@ -120,12 +132,13 @@ class BHM_PYTORCH():
             y_min, y_max = max_min[2], max_min[3]
             z_min, z_max = max_min[4], max_min[5]
 
+        print(f"grid points x: {x_min} {x_max} y: {y_min} {y_max} z: {z_min} {z_max}")
+
         xx, yy, zz = pt.meshgrid([pt.arange(x_min, x_max, cell_resolution[0], device=self.device),
                               pt.arange(y_min, y_max, cell_resolution[1], device=self.device),
                               pt.arange(z_min, z_max, cell_resolution[2], device=self.device)], indexing="ij")
         grid = pt.stack((xx.reshape(-1,1), yy.reshape(-1,1), zz.reshape(-1,1)), dim=1).squeeze()
         return grid
-
 
     def __sparse_features(self, X):
         """
@@ -136,7 +149,7 @@ class BHM_PYTORCH():
             rbf_features, _, _ = self.rbf_torch.eval(X, self.grid, gamma=self.gamma)
             return rbf_features
         else:
-            rbf_features = rbf_kernel(X.cpu(), self.grid.cpu(), gamma=self.gamma)
+            rbf_features = rbf_kernel(X.detach().cpu().numpy(), self.grid.detach().cpu().numpy(), gamma=self.gamma)
             # COMMENTED OUT BIAS TERM
             # rbf_features = np.hstack((np.ones(X.shape[0])[:, np.newaxis], rbf_features))
             return pt.tensor(rbf_features, device=self.device)
@@ -169,10 +182,11 @@ class BHM_PYTORCH():
 
     def load(self, file_path=None):
         params_dict = pt.load(file_path)
+        print(params_dict)
         self.gamma = params_dict['gamma']
-        self.grid = params_dict['grid']
-        self.mu = params_dict['mu']
-        self.sig = params_dict['sig']
+        self.grid = params_dict['grid'].to(self.device)
+        self.mu = params_dict['mu'].to(self.device)
+        self.sig = params_dict['sig'].to(self.device)
         self.nIter = params_dict['nIter']
 
     def fit(self, X, y):
@@ -189,15 +203,14 @@ class BHM_PYTORCH():
             self.sig = 10000 * pt.ones(D, device=self.device)
 
         for i in range(self.nIter):
-            if VERBOSE:
-                print(f"  Parameter estimation: iter={i}")
 
             # E-step
             self.mu, self.sig = self.__calc_posterior(X, y, self.epsilon, self.mu, self.sig)
 
             # M-step
             self.epsilon = pt.sqrt(pt.sum((X**2)*self.sig, dim=1) + (X.mm(self.mu.reshape(-1, 1))**2).squeeze())
-
+            if VERBOSE:
+                print(f"  Parameter estimation: iter={i} \t mu: {self.mu.size()} sig: {self.sig.size()} x: {X.size()}")
         # print(self.mu)
 
         return self.mu, self.sig
@@ -226,6 +239,25 @@ class BHM_PYTORCH():
         sig2_inv_a = pt.sum((K ** 2) * self.sig, dim=1)
         k = 1.0 / pt.sqrt(1 + np.pi * sig2_inv_a / 8)
         log_p = pt.log(1. - pt.sigmoid(k * mu_a))
+
+        # TODO: add the limits alterations to log_p calculations?
+        if self.limits is not None:
+            # print(self.limits[0][0])
+            # print(Xq[0,0])
+            print_str = f"log_p: {log_p[0]:5.6f}"
+            
+            scale = 1.
+            log_p_diff = pt.exp(-scale*(Xq[:, 0] - self.limits[0][0]))
+            log_p_diff += pt.exp( scale*(Xq[:, 0] - self.limits[0][1]))
+            log_p_diff += pt.exp(-scale*(Xq[:, 1] - self.limits[1][0]))
+            log_p_diff += pt.exp( scale*(Xq[:, 1] - self.limits[1][1]))
+            if len(self.limits) == 3:
+                log_p_diff += pt.exp(-scale*(Xq[:, 2] - self.limits[2][0]))
+                log_p_diff += pt.exp( scale*(Xq[:, 2] - self.limits[2][1]))            
+            print(print_str + f" diff: {log_p_diff[0]:5.6f} new log_p: {log_p[0] - log_p_diff[0]:5.6f}  x: {Xq[0,0] - self.limits[0][0]:5.6f} {Xq[0,0] - self.limits[0][1]:5.6f} y: {Xq[0,1] - self.limits[1][0]:5.6f} {Xq[0,1] - self.limits[1][1]:5.6f} z: {Xq[0,2] - self.limits[2][0]:5.6f} {Xq[0,2] - self.limits[2][1]:5.6f}")
+            log_p -= log_p_diff
+        else:
+            print(f"log_p: {log_p[0]:5.6f}")
 
         # Autodiff second term.
         dlog_p_dK = pt.autograd.grad(log_p.sum(), K,)[0] # batch x rbf_features
